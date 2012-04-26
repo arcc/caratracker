d9d6d1cc5cef8079094b0b6a14d04924d8e7e8c3
Anthony Ford <ford.anthonyj@gmail.com>
2012-04-25 22:09:07 -0500

    Fix - forgot another import. fixes #35

--- tracker/utils.py
+++ tracker/utils.py
@@ -21,7 +21,7 @@ def delete_ticket_cascade(ticket):
     for note in ticket.notes:
         models.db.session.delete(note)
     for f in ticket.files:
-        unlink(path.join(forms.upload_dir,f.filename))
+        unlink(path.join(forms.upload_dir(app),f.filename))
         models.db.session.delete(f)
     models.db.session.commit()
     models.db.session.delete(ticket)
